f21473852e4e9c7be15db239440871c5a7d7250f
BuildTools <unconfigured@null.spigotmc.org>
2023-02-28 17:00:26 -0500

    Teaching Content

--- app.py
+++ app.py
@@ -2,39 +2,51 @@ from flask import Flask, session, render_template, Response, request, request, u
 
 app = Flask(__name__)
 appName = "QTMA Product Series"
+pageName = ""
+
+app.config['SECRET_KEY'] = 'AF92AF84692AE0661B1CE20B'
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'},
+            {'title': 'Message Three',
+             'content': 'Message Three Content'},
+            ]
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        logo = request.form['logo']
-        length = request.form['length']
-
-        if not prompt:
-            flash('Prompt is required!')
-        elif not logo:
-            flash('Logo is required!')
-        elif not length:
-            flash('Time is required!')
-        else:
-            length = int(re.search(r'\d+', length).group())
-            length = (length - 2) * 40
-
-            script = make_script(prompt, length, api_key)
-            slides = get_slide_points(script, api_key)
-            long_script = write_script_to_single_page(script)
-            img_url = generate_logo(logo, api_key)
-            print("Processing")
-            return render_template('product.html', appName=appName, slides=slides, script=script, long_script=long_script, img_url=img_url)
-
-    elif request.method == 'GET':
-        return render_template('index.html', appName=appName)
-
+    # if request.method == 'POST':
+    #     if request.form.get('btn-fs') == 'FULL_STACK':
+    #         pageName = "Full-Stack Development"
+    #     elif request.form.get('btn-do') == 'DATA_OPTIMIZATION':
+    #         pageName = "Data Optimization"
+    #     else:
+    #         pass
+    # elif request.method == 'GET':
+    #     return render_template('index.html', appName=appName)
     return render_template('index.html')
 
-@app.route('/product/', methods=('GET', 'POST'))
-def product():
-    return render_template('product.html', appName=appName, slides=slides, script=script, long_script=long_script, img_url=img_url)
+@app.route('/fullstack', methods=('GET', 'POST'))
+def fullstack():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title and not content:
+            flash('Title and content are empty!')
+        elif not title:
+            flash('Title is empty!')
+        elif not content:
+            flash('Content is empty!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('fullstack'))
+    return render_template('fullstack.html', appName=appName, messages=messages)
+
+@app.route('/dataop', methods=('GET', 'POST'))
+def dataop():
+    return render_template('dataop.html', appName=appName)
 
 if __name__ == "__main__":
   app.run(debug=True)
